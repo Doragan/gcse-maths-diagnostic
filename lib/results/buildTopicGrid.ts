34683cf3ba9e6c193f20e46b0eab1d5fca050217
Christopher Reay <christopher.reay89@gmail.com>
2026-03-20 15:34:47 +0000

    fix stuff

--- lib/results/buildTopicGrid.ts
+++ lib/results/buildTopicGrid.ts
@@ -1,26 +1,24 @@
-export function buildTopicGrid(topicSkills, mastered, needsPractice) {
-
+export function buildTopicGrid(
+  topicSkills: Record<string, string[]>,
+  mastered: Set<string>,
+  needsPractice: Set<string>
+) {
   return Object.entries(topicSkills).map(([topic, skillIds]) => {
+    const masteredSkills: string[] = [];
+    const needsPracticeSkills: string[] = [];
+    const untestedSkills: string[] = [];
 
-    const masteredSkills = [];
-    const needsPracticeSkills = [];
-    const untestedSkills = [];
-
-    skillIds.forEach(id => {
-
+    skillIds.forEach((id: string) => {
       if (mastered.has(id)) masteredSkills.push(id);
       else if (needsPractice.has(id)) needsPracticeSkills.push(id);
       else untestedSkills.push(id);
-
     });
 
     return {
       topic,
       masteredSkills,
       needsPracticeSkills,
-      untestedSkills
+      untestedSkills,
     };
-
   });
-
 }
